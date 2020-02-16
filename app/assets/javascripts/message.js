bfe7c13c9a366b0e984bdf762357e20960527695
hasumin71 <60636877+hasumin71@users.noreply.github.com>
2020-02-16 15:25:34 +0900

    incremental-search-maked

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,59 +1,40 @@
 $(function(){ 
 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+    image = ( message.image ) ? `<img class="lower-message__image" src=${message.image} >` : ""; 
+                var html =  
+                ` <div class="message" data-message-id="${message.id}">
+                <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.date}
+                  </div>
+                </div>
+                <div class="lower-message">
+                  <p class="lower-message__content">
+                  ${message.content}
+                  </p>
+                  ${image}
+                </div>
+              </div>`
+          return html; 
+  }
 
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
 
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
 
   .done(function(data){
     var html = buildHTML(data);
@@ -71,4 +52,36 @@ $('#new_message').on('submit', function(e){
   prop("disabled", false);  
   });
 })
-});
+
+
+
+  var reloadMessages = function() {
+      var last_message_id = $('.message:last').data("message-id"); 
+      $.ajax({
+        url: "api/messages", 
+        type: 'get',
+        dataType: 'json', 
+        data: {id: last_message_id}
+      })
+      .done(function(messages){ 
+        if (messages.length !== 0) {
+          var insertHTML = ''; 
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          })
+            $(".messages").append(insertHTML); 
+            $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        }  
+      })
+
+      .fail(function(){ 
+        alert('自動更新に失敗しました'); 
+      })
+  }
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+});  
+
+
+
